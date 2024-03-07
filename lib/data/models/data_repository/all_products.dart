6bd59ba3ef494cc4e2d52f2d87d244808eab25a8
Flutter <aziz2311e@gmail.com>
2024-03-07 22:08:29 +0300

    ds

--- lib/data/models/data_repository/all_products.dart
+++ lib/data/models/data_repository/all_products.dart
@@ -1,27 +1,52 @@
 import 'package:task/utils/app_images/app_images.dart';
 
+import '../coffee_category_model.dart';
 import '../coffee_model/coffee_model.dart';
+
+List<CoffeeCategoryModel> categories = [
+  CoffeeCategoryModel(
+    id: 1,
+    name: "Mocha",
+  ),
+  CoffeeCategoryModel(
+    id: 2,
+    name: "Latte",
+  ),
+  CoffeeCategoryModel(
+    id: 3,
+    name: "Cappucino",
+  ),
+  CoffeeCategoryModel(
+    id: 4,
+    name: "Espresso",
+  ),
+];
 
 List<CoffeeModel> allProducts = [
   // ========================== mocha coffee =====================//
   CoffeeModel(
-      name: "Mocha",
-      description:
-          "Although a mocha is often interpreted differently across the world, the basis is that a shot of espresso is combined with a chocolate powder or syrup, followed by milk or cream. It is a variant of a latte, in the sense that it is often 1/3 espresso and 2/3 steamed milk. However, a chocolate flavour is added, and this can be milk or dark.",
-      subtitle: "Caramel Mocha",
-      price: 120,
-      type: CoffeeType.s,
-      count: 1,
-      imageUrl: AppImages.coffeeOne),
+    name: "Mocha",
+    description:
+        "Although a mocha is often interpreted differently across the world, the basis is that a shot of espresso is combined with a chocolate powder or syrup, followed by milk or cream. It is a variant of a latte, in the sense that it is often 1/3 espresso and 2/3 steamed milk. However, a chocolate flavour is added, and this can be milk or dark.",
+    subtitle: "Caramel Mocha",
+    price: 120,
+    type: CoffeeType.s.toString(),
+    imagePath: AppImages.coffeeOne,
+    rate: 4.6,
+    categoryId: 1,
+    isFavourite: false,
+  ),
   CoffeeModel(
     name: "Mocha",
     description:
         "Although a mocha is often interpreted differently across the world, the basis is that a shot of espresso is combined with a chocolate powder or syrup, followed by milk or cream. It is a variant of a latte, in the sense that it is often 1/3 espresso and 2/3 steamed milk. However, a chocolate flavour is added, and this can be milk or dark.",
     subtitle: "With Oat Milk",
     price: 130,
-    type: CoffeeType.s,
-    imageUrl: AppImages.coffeeTwo,
-    count: 1,
+    type: CoffeeType.s.toString(),
+    imagePath: AppImages.coffeeTwo,
+    rate: 4.6,
+    categoryId: 1,
+    isFavourite: false,
   ),
   CoffeeModel(
     name: "Mocha",
@@ -29,9 +54,11 @@ List<CoffeeModel> allProducts = [
         "Although a mocha is often interpreted differently across the world, the basis is that a shot of espresso is combined with a chocolate powder or syrup, followed by milk or cream. It is a variant of a latte, in the sense that it is often 1/3 espresso and 2/3 steamed milk. However, a chocolate flavour is added, and this can be milk or dark.",
     subtitle: "With chocolate",
     price: 140,
-    type: CoffeeType.s,
-    count: 1,
-    imageUrl: AppImages.coffeeThree,
+    type: CoffeeType.s.toString(),
+    rate: 4.6,
+    categoryId: 1,
+    isFavourite: false,
+    imagePath: AppImages.coffeeThree,
   ),
   // ========================== latte coffee =====================//
   CoffeeModel(
@@ -40,28 +67,35 @@ List<CoffeeModel> allProducts = [
         "A latte or caffè latte is a milk coffee that boasts a silky layer of foam as a real highlight to the drink. A true latte will be made up of one or two shots of espresso, steamed milk and a final, thin layer of frothed milk on top.",
     subtitle: "Caramel Latte",
     price: 120,
-    type: CoffeeType.s,
-    imageUrl: AppImages.img1,
-    count: 1,
+    type: CoffeeType.s.toString(),
+    rate: 4.6,
+    categoryId: 2,
+    isFavourite: false,
+    imagePath: AppImages.img1,
   ),
   CoffeeModel(
-      name: "Latte",
-      description:
-          "A latte or caffè latte is a milk coffee that boasts a silky layer of foam as a real highlight to the drink. A true latte will be made up of one or two shots of espresso, steamed milk and a final, thin layer of frothed milk on top.",
-      subtitle: "With Oat Milk",
-      price: 130,
-      type: CoffeeType.s,
-      count: 1,
-      imageUrl: AppImages.img2),
+    name: "Latte",
+    description:
+        "A latte or caffè latte is a milk coffee that boasts a silky layer of foam as a real highlight to the drink. A true latte will be made up of one or two shots of espresso, steamed milk and a final, thin layer of frothed milk on top.",
+    subtitle: "With Oat Milk",
+    price: 130,
+    rate: 4.6,
+    categoryId: 2,
+    isFavourite: false,
+    type: CoffeeType.s.toString(),
+    imagePath: AppImages.img2,
+  ),
   CoffeeModel(
     name: "Latte",
     description:
         "A latte or caffè latte is a milk coffee that boasts a silky layer of foam as a real highlight to the drink. A true latte will be made up of one or two shots of espresso, steamed milk and a final, thin layer of frothed milk on top.",
     subtitle: "With chocolate",
     price: 130,
-    type: CoffeeType.s,
-    count: 1,
-    imageUrl: AppImages.img3,
+    type: CoffeeType.s.toString(),
+    rate: 4.6,
+    categoryId: 2,
+    isFavourite: false,
+    imagePath: AppImages.img3,
   ),
 
   // ========================== cappucino coffee =====================//
@@ -71,9 +105,11 @@ List<CoffeeModel> allProducts = [
         "A cappuccino is the perfect balance of espresso, steamed milk and foam. This coffee is all about the structure and the even splitting of all elements into equal thirds.",
     subtitle: "Caramel Cappucino",
     price: 120,
-    type: CoffeeType.s,
-    count: 1,
-    imageUrl: AppImages.img4,
+    type: CoffeeType.s.toString(),
+    imagePath: AppImages.img4,
+    rate: 4.6,
+    categoryId: 3,
+    isFavourite: false,
   ),
   CoffeeModel(
     name: "Cappucino",
@@ -81,9 +117,11 @@ List<CoffeeModel> allProducts = [
         "A cappuccino is the perfect balance of espresso, steamed milk and foam. This coffee is all about the structure and the even splitting of all elements into equal thirds.",
     subtitle: "With Oat Milk",
     price: 130,
-    type: CoffeeType.s,
-    count: 1,
-    imageUrl: AppImages.img5,
+    type: CoffeeType.s.toString(),
+    rate: 4.6,
+    categoryId: 3,
+    isFavourite: false,
+    imagePath: AppImages.img5,
   ),
   CoffeeModel(
     name: "Cappucino",
@@ -91,9 +129,11 @@ List<CoffeeModel> allProducts = [
         "A cappuccino is the perfect balance of espresso, steamed milk and foam. This coffee is all about the structure and the even splitting of all elements into equal thirds.",
     subtitle: "With chocolate",
     price: 140,
-    type: CoffeeType.s,
-    count: 1,
-    imageUrl: AppImages.img6,
+    type: CoffeeType.s.toString(),
+    rate: 4.6,
+    categoryId: 3,
+    isFavourite: false,
+    imagePath: AppImages.img6,
   ),
 
   // ========================== Espresso coffee =====================//
@@ -103,9 +143,11 @@ List<CoffeeModel> allProducts = [
         "As a beverage, espresso is a concentrated form of coffee brewed with high pressure, hot water and finely ground coffee beans.",
     subtitle: "With chocolate",
     price: 120,
-    type: CoffeeType.s,
-    count: 1,
-    imageUrl: AppImages.img7,
+    type: CoffeeType.s.toString(),
+    rate: 4.6,
+    categoryId: 4,
+    isFavourite: false,
+    imagePath: AppImages.img7,
   ),
   CoffeeModel(
     name: "Espresso",
@@ -113,9 +155,11 @@ List<CoffeeModel> allProducts = [
         "As a beverage, espresso is a concentrated form of coffee brewed with high pressure, hot water and finely ground coffee beans.",
     subtitle: "With Oat Milk",
     price: 130,
-    type: CoffeeType.s,
-    count: 1,
-    imageUrl: AppImages.img8,
+    type: CoffeeType.s.toString(),
+    rate: 4.6,
+    categoryId: 4,
+    isFavourite: false,
+    imagePath: AppImages.img8,
   ),
   CoffeeModel(
     name: "Espresso",
@@ -123,8 +167,16 @@ List<CoffeeModel> allProducts = [
         "As a beverage, espresso is a concentrated form of coffee brewed with high pressure, hot water and finely ground coffee beans.",
     subtitle: "Caramel Espresso",
     price: 140,
-    type: CoffeeType.s,
-    count: 1,
-    imageUrl: AppImages.img9,
+    type: CoffeeType.s.toString(),
+    rate: 4.6,
+    categoryId: 4,
+    isFavourite: false,
+    imagePath: AppImages.img9,
   ),
 ];
+
+enum CoffeeType {
+  s,
+  m,
+  l,
+}
